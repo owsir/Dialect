8ca8ee3642b30bc6e11deab451ce6793a7a3326b
David Chen <david.chen@wendywuchina.com>
2018-01-22 11:47:55 +0800

    Implement editor

--- Dialect.Web/App_Start/BundleConfig.cs
+++ Dialect.Web/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace Dialect.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/summernote.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/summernote.css"));
         }
     }
 }
