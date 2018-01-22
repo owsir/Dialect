5e837b663f01c3667c70b322894bd2404fe6df27
David Chen <david.chen@wendywuchina.com>
2018-01-22 17:35:25 +0800

    Insert editor

--- Dialect.Web/Controllers/ForumPostController.cs
+++ Dialect.Web/Controllers/ForumPostController.cs
@@ -20,23 +20,18 @@ namespace Dialect.Web.Controllers
 
         public ActionResult Index(int id)
         {
-            var post = _forumPostLogic.GetForumPostByPostId(id);
-            var postReplies = _postReplyLogic.GetPostRepliesByPostId(id);
-            var viewModel = new ForumPostViewModel
-            {
-                ForumPost = post,
-                PostReplies = postReplies,
-                ReplyObjectViewModel = new ReplyObjectViewModel
-                {
-                    PostId = id
-                }
-            };
-            return View(viewModel);
+            return View(GetPostAndRepliesById(id));
         }
 
+        [ValidateInput(false)]
         [HttpPost]
         public ActionResult NewReply(ReplyObjectViewModel replyObjectViewModel)
         {
+            if (string.IsNullOrEmpty(replyObjectViewModel.Content))
+            {
+                ModelState.AddModelError("", "内容不能为空.");
+                return View("Index", GetPostAndRepliesById(replyObjectViewModel.PostId));
+            }
             var replyModel=new PostReply()
             {
                 Content = replyObjectViewModel.Content,
@@ -47,5 +42,22 @@ namespace Dialect.Web.Controllers
             _postReplyLogic.InsertPostReply(replyModel);
             return RedirectToAction("Index", "ForumPost", new { id = replyObjectViewModel.PostId });
         }
+
+        private ForumPostViewModel GetPostAndRepliesById(int id)
+        {
+            var post = _forumPostLogic.GetForumPostByPostId(id);
+            var postReplies = _postReplyLogic.GetPostRepliesByPostId(id);
+            var postAndReplies = new ForumPostViewModel
+            {
+                ForumPost = post,
+                PostReplies = postReplies,
+                ReplyObjectViewModel = new ReplyObjectViewModel
+                {
+                    PostId = id
+                }
+            };
+            return postAndReplies;
+        }
+
     }
 }
